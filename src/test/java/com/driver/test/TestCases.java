10bbebda2b3962adee4170907d3044374dd0d630
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-10 10:11:40 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,10 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+    // Task 5: Add a test case to test the overridden method from object of class B
+    @Test
+    public void testprintHelloWorld2() {
+        Main.B objB = new Main.B();
+        assertEquals("Method is overridden in Extended class B", objB.meth());
+    }
 }
